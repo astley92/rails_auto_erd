6e60677cb355485cab6f0ba1d0bcd2232a9a9d74
Blake Astley <astley92@hotmail.com>
2024-06-11 20:32:00 +1000

    Init

---
+++ lib/rails_auto_erd.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rails_auto_erd/version"
+
+module RailsAutoErd
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/rails_auto_erd_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe RailsAutoErd do
+  it "has a version number" do
+    expect(RailsAutoErd::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
