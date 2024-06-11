e165029fc274d50fbfc93f752c02d0ddb1f27e3a
Blake Astley <astley92@hotmail.com>
2024-06-11 21:12:02 +1000

    Remove auto generated spec

--- spec/rails_auto_erd_spec.rb
+++ spec/rails_auto_erd_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe RailsAutoErd do
   it "has a version number" do
     expect(RailsAutoErd::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
